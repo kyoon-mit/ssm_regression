cadd926ef4e6f2db74663bd0e81edfab4befaad6
Kyungseop Yoon <kyoon@mit.edu>
2025-06-24 16:50:27 -0400

    organized into class

--- embedding.py
+++ embedding.py
@@ -6,128 +6,151 @@ from torch import nn, optim
 import wandb
 from datetime import datetime
 
-timestamp = datetime.now().strftime('%y%m%d%H%M%S')
+class Embedding():
+    def __init__(
+        self,
+        num_hidden_layers_h=2, # number of hidden layers in the embedding model
+        device=None,
+        datatype='SHO', # options: 'SineGaussian', 'SHO', 'LIGO'
+        datasfx='', # suffix for the dataset, e.g., '_sigma0.4_gaussian'
+    ):
 
-datatype = 'SineGaussian'
+        # Load datasets
+        if datatype=='SineGaussian':
+            from data_sinegaussian import DataGenerator
+        elif datatype=='SHO':
+            from data_sho import DataGenerator
+        elif datatype=='LIGO':
+            pass # TODO: implement LIGO data loading
 
-os.environ['WANDB_MODE'] = 'offline'
-wandb.init(project=f'embedding_{datatype}', name=f'embedding_{datatype}_{timestamp}')
+        if device is None:
+            self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        else:
+            self.device = torch.device(device)
+        print(f"Using device={self.device}")
+        
+        self.datatype = datatype
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Using {device=}")
+        self.datadir = f'/ceph/submit/data/user/k/kyoon/KYoonStudy/models/{datatype}'
+        self.modeldir = os.path.join(self.datadir, 'output')
 
-# datadir = '/n/holystore01/LABS/iaifi_lab/Users/creissel/SHO/'
-# modeldir = '/n/holystore01/LABS/iaifi_lab/Users/creissel/SHO/models/'
-datadir = f'/ceph/submit/data/user/k/kyoon/KYoonStudy/models/{datatype}'
-modeldir = os.path.join(datadir, 'output')
+        self.train_dict = torch.load(os.path.join(self.datadir, f'train{datasfx}.pt'), map_location=device, weights_only=True)
+        self.val_dict = torch.load(os.path.join(self.datadir, f'val{datasfx}.pt'), map_location=device, weights_only=True)
 
-# Load datasets
-from data_sinegaussian import DataGenerator
+        self.train_data = DataGenerator(self.train_dict)
+        self.val_data = DataGenerator(self.val_dict)
 
-train_dict = torch.load(os.path.join(datadir, 'train.pt'), map_location=device, weights_only=True)
-val_dict = torch.load(os.path.join(datadir, 'val.pt'), map_location=device, weights_only=True)
+        self.TRAIN_BATCH_SIZE = 1000 
+        self.VAL_BATCH_SIZE = 1000
 
-train_data = DataGenerator(train_dict)
-val_data = DataGenerator(val_dict)
-
-TRAIN_BATCH_SIZE = 1000 
-VAL_BATCH_SIZE = 1000 
-train_data_loader = DataLoader(
-    train_data, batch_size=TRAIN_BATCH_SIZE,
-    shuffle=True
-)
-val_data_loader = DataLoader(
-    val_data, batch_size=VAL_BATCH_SIZE,
-    shuffle=True
-)
-
-print('Finished loading data! Start loading model...')
-
-# Load loss and model
-from losses import VICRegLoss
-from models import SimilarityEmbedding
-vicreg_loss = VICRegLoss()
-similarity_embedding = SimilarityEmbedding(num_hidden_layers_h=2).to(device)
-# setup scheduler
-optimizer = optim.Adam(similarity_embedding.parameters(), lr=5e-3)
-scheduler_1 = optim.lr_scheduler.ConstantLR(optimizer, total_iters=20)
-scheduler_2 = optim.lr_scheduler.OneCycleLR(optimizer, total_steps=20, max_lr=5e-4)
-scheduler_3 = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.99)
-scheduler = optim.lr_scheduler.SequentialLR(
-    optimizer, schedulers=[scheduler_1, scheduler_2, scheduler_3], milestones=[20, 40])
-
-print('...done loading model!')
-
-# print number of trainable parameters
-sum_param=0
-for name, param in similarity_embedding.named_parameters():
-    if param.requires_grad:
-        sum_param+=param.numel()
-print('trainable parameters:',sum_param)
-
-# Definition of training cycle
-def train_one_epoch(epoch_index, **vicreg_kwargs):
-    running_sim_loss = 0.
-    last_sim_loss = 0.
-
-    #for idx, val in enumerate(tqdm(train_data_loader, desc='train', leave=False), 1):
-    for idx, val in enumerate(train_data_loader):
-        augmented_theta, _, augmented_data, unshifted_data = val
-        embedded_values_aug, _ = similarity_embedding(augmented_data)
-        embedded_values_orig, _ = similarity_embedding(unshifted_data)
-        sim_loss = 0
-
-        similar_embedding_loss, _repr, _cov, _std = vicreg_loss(
-            embedded_values_aug,
-            embedded_values_orig,
-            **vicreg_kwargs
+        self.train_data_loader = DataLoader(
+            self.train_data, batch_size=self.TRAIN_BATCH_SIZE,
+            shuffle=True
         )
-        sim_loss += similar_embedding_loss
-        optimizer.zero_grad()
-        sim_loss.backward()
-        optimizer.step()
-        # Gather data and report
-        running_sim_loss += sim_loss.item()
-        if idx % 10 == 0:
-            last_sim_loss = running_sim_loss / 10
-            tb_x = epoch_index * len(train_data_loader) + idx
-            #tb_writer.add_scalar('SimLoss/train', last_sim_loss, tb_x)
-            print('SimLoss/train', last_sim_loss, tb_x)
-            running_sim_loss = 0.
-    return last_sim_loss
-
-
-def val_one_epoch(epoch_index, **vicreg_kwargs):
-    running_sim_loss = 0.
-    last_sim_loss = 0.
-
-    #for idx, val in enumerate(tqdm(val_data_loader, desc='val', leave=False), 1):
-    for idx, val in enumerate(val_data_loader):
-        augmented_theta, _, augmented_data, unshifted_data = val
-        embedded_values_aug, _ = similarity_embedding(augmented_data)
-        embedded_values_orig, _ = similarity_embedding(unshifted_data)
-        sim_loss = 0
-
-        similar_embedding_loss, _repr, _cov, _std = vicreg_loss(
-            embedded_values_aug,
-            embedded_values_orig,
-            **vicreg_kwargs
+        self.val_data_loader = DataLoader(
+            self.val_data, batch_size=self.VAL_BATCH_SIZE,
+            shuffle=True
         )
-        sim_loss += similar_embedding_loss
 
-        running_sim_loss += sim_loss.item()
-        if idx % 5 == 0:
-            last_sim_loss = running_sim_loss / 5
-            tb_x = epoch_index * len(val_data_loader) + idx + 1
-            #tb_writer.add_scalar('SimLoss/val', last_sim_loss, tb_x)
-            print('SimLoss/val', last_sim_loss, tb_x)
-            #tb_writer.flush()
-            print(f'Last {_repr.item():.2f}; {_cov.item():.2f}; {_std.item():.2f}')
-            running_sim_loss = 0.
-    #tb_writer.flush()
-    return last_sim_loss
+        self.model = None
+        self.vicreg_loss = None
+        self.num_hidden_layers_h = num_hidden_layers_h
+        self.optimizer, self.scheduler = None, None
+
+    def build_model(self):
+        print('==> Building embedding model...')
+        # Load loss and model
+        from losses import VICRegLoss
+        from models import SimilarityEmbedding
+        self.vicreg_loss = VICRegLoss()
+        self.model = SimilarityEmbedding(num_hidden_layers_h=self.num_hidden_layers_h).to(self.device)
+        # setup scheduler
+        self.optimizer = optim.Adam(self.model.parameters(), lr=5e-3)
+        scheduler_1 = optim.lr_scheduler.ConstantLR(self.optimizer, total_iters=20)
+        scheduler_2 = optim.lr_scheduler.OneCycleLR(self.optimizer, total_steps=20, max_lr=5e-4)
+        scheduler_3 = optim.lr_scheduler.ExponentialLR(self.optimizer, gamma=0.99)
+        self.scheduler = optim.lr_scheduler.SequentialLR(
+        self.optimizer, schedulers=[scheduler_1, scheduler_2, scheduler_3], milestones=[20, 40])
+        print('...done!')
+
+        # print number of trainable parameters
+        sum_param=0
+        for _, param in self.model.named_parameters():
+            if param.requires_grad:
+                sum_param+=param.numel()
+        print(f'trainable parameters: {sum_param}')
+
+    # Definition of training cycle
+    def train_one_epoch(self, epoch_index, **vicreg_kwargs):
+        running_sim_loss = 0.
+        last_sim_loss = 0.
+
+        #for idx, val in enumerate(tqdm(train_data_loader, desc='train', leave=False), 1):
+        for idx, val in enumerate(self.train_data_loader):
+            _, _, unshifted_data, augmented_data, _, _, _, _, _, _ = val
+            embedded_values_aug, _ = self.model(augmented_data)
+            embedded_values_orig, _ = self.model(unshifted_data)
+            sim_loss = 0
+
+            similar_embedding_loss, _repr, _cov, _std = self.vicreg_loss(
+                embedded_values_aug,
+                embedded_values_orig,
+                **vicreg_kwargs
+            )
+            sim_loss += similar_embedding_loss
+            self.optimizer.zero_grad()
+            sim_loss.backward()
+            self.optimizer.step()
+            # Gather data and report
+            running_sim_loss += sim_loss.item()
+            if idx % 10 == 0:
+                last_sim_loss = running_sim_loss / 10
+                tb_x = epoch_index * len(self.train_data_loader) + idx
+                #tb_writer.add_scalar('SimLoss/train', last_sim_loss, tb_x)
+                print('SimLoss/train', last_sim_loss, tb_x)
+                running_sim_loss = 0.
+        return last_sim_loss
+
+    def val_one_epoch(self, epoch_index, **vicreg_kwargs):
+        running_sim_loss = 0.
+        last_sim_loss = 0.
+
+        #for idx, val in enumerate(tqdm(val_data_loader, desc='val', leave=False), 1):
+        for idx, val in enumerate(self.val_data_loader):
+            augmented_theta, _, augmented_data, unshifted_data = val
+            embedded_values_aug, _ = self.model(augmented_data)
+            embedded_values_orig, _ = self.model(unshifted_data)
+            sim_loss = 0
+
+            similar_embedding_loss, _repr, _cov, _std = self.vicreg_loss(
+                embedded_values_aug,
+                embedded_values_orig,
+                **vicreg_kwargs
+            )
+            sim_loss += similar_embedding_loss
+
+            running_sim_loss += sim_loss.item()
+            if idx % 5 == 0:
+                last_sim_loss = running_sim_loss / 5
+                tb_x = epoch_index * len(self.val_data_loader) + idx + 1
+                #tb_writer.add_scalar('SimLoss/val', last_sim_loss, tb_x)
+                print('SimLoss/val', last_sim_loss, tb_x)
+                #tb_writer.flush()
+                print(f'Last {_repr.item():.2f}; {_cov.item():.2f}; {_std.item():.2f}')
+                running_sim_loss = 0.
+        #tb_writer.flush()
+        return last_sim_loss
 
 if __name__=='__main__':
+
+    datatype = 'SHO'
+    timestamp = datetime.now().strftime('%y%m%d%H%M%S')
+
+    os.environ['WANDB_MODE'] = 'offline'
+    wandb.init(project=f'embedding_{datatype}', name=f'embedding_{datatype}_{timestamp}')
+
+    task = Embedding(datatype=datatype, datasfx='_sigma0.4_gaussian')
+    task.build_model()
 
     print('Start training...')
 
@@ -144,12 +167,12 @@ if __name__=='__main__':
 
         print(f"VicReg wts: {wt_repr=} {wt_cov=} {wt_std=}")
         # Gradient tracking
-        similarity_embedding.train(True)
-        avg_train_loss = train_one_epoch(epoch_number, wt_repr=wt_repr, wt_cov=wt_cov, wt_std=wt_std)
+        task.model.train(True)
+        avg_train_loss = task.train_one_epoch(epoch_number, wt_repr=wt_repr, wt_cov=wt_cov, wt_std=wt_std)
 
         # no gradient tracking, for validation
-        similarity_embedding.train(False)
-        avg_val_loss = val_one_epoch(epoch_number, wt_repr=wt_repr, wt_cov=wt_cov, wt_std=wt_std)
+        task.model.train(False)
+        avg_val_loss = task.val_one_epoch(epoch_number, wt_repr=wt_repr, wt_cov=wt_cov, wt_std=wt_std)
 
         print(f"Train/Val Sim Loss after epoch: {avg_train_loss:.4f}/{avg_val_loss:.4f}")
 
@@ -157,7 +180,7 @@ if __name__=='__main__':
             'epoch': epoch_number,
             'train_loss': avg_train_loss,
             'val_accuracy': avg_val_loss,
-            'lr': optimizer.param_groups[0]['lr'],
+            'lr': task.optimizer.param_groups[0]['lr'],
             'wt_repr': wt_repr,
             'wt_cov': wt_cov,
             'wt_std': wt_std
@@ -165,10 +188,10 @@ if __name__=='__main__':
 
         epoch_number += 1
         try:
-            scheduler.step(avg_val_loss)
+            task.scheduler.step(avg_val_loss)
         except TypeError:
-            scheduler.step()
+            task.scheduler.step()
 
     wandb.finish()
 
-    torch.save(similarity_embedding.state_dict(), os.path.join(modeldir, f'model.CNN.{datatype}.{timestamp}.path'))
+    torch.save(task.model.state_dict(), os.path.join(task.modeldir, f'model.CNN.{datatype}.{timestamp}.path'))
