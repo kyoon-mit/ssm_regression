067b934b331789082e15ce125d00cdaf3db00a67
Kyungseop Yoon <kyoon@mit.edu>
2025-08-25 13:46:12 -0400

    removing unnecessary files

--- slurm/bash_scripts/sho_run_bilby_id05000-05999.sh
+++
@@ -1,5 +0,0 @@
-source /work/submit/kyoon/miniforge3/etc/profile.d/conda.sh
-conda activate ssm
-BASE_DIR="/ceph/submit/data/user/k/kyoon/KYoonStudy"
-python /ceph/submit/data/user/k/kyoon/KYoonStudy/ssm_regression/call_fitting.py -t SHO -j bilby -b 5000 6000 \
---logfile="${BASE_DIR}/fitresults/bilby_sho/id05000-05999.log"
--- slurm/bash_scripts/sho_run_bilby_id07000-07999.sh
+++
@@ -1,5 +0,0 @@
-source /work/submit/kyoon/miniforge3/etc/profile.d/conda.sh
-conda activate ssm
-BASE_DIR="/ceph/submit/data/user/k/kyoon/KYoonStudy"
-python /ceph/submit/data/user/k/kyoon/KYoonStudy/ssm_regression/call_fitting.py -t SHO -j bilby -b 7000 8000 \
---logfile="${BASE_DIR}/fitresults/bilby_sho/id07000-07999.log"
